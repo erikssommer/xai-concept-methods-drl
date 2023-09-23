10afd47a3565bd29e4117d396c09d0ab2951df5c
Erik Storås Sommer <erikssommer@gmail.com>
2023-09-23 23:18:38 +0200

    Concepts to detect in policy nn

---
+++ concepts/concepts.py
@@ -0,0 +1,89 @@
+def concept_eyes():
+    """
+    In the game of Go, an eye is a group of empty points surrounded by stones of a single color,
+    such that no opposing stone can be placed in the group without being captured. Eyes are important
+    because they allow a group of stones to have multiple liberties, making it more difficult for the
+    opponent to capture the group.
+    """
+    pass
+
+def concept_atari():
+    """
+    In the game of Go, atari is a situation in which a group of stones has only one liberty remaining.
+    If the opponent places a stone on that liberty, the group will be captured. Atari is an important
+    concept because it can be used to force the opponent to defend their stones, or to create a ko
+    situation in which the opponent is not allowed to recapture the group immediately.
+    """
+    pass
+
+def concept_ko():
+    """
+    In the game of Go, ko is a situation in which a group of stones can be captured, but the opponent
+    is not allowed to recapture the group immediately. Ko is an important concept because it can be
+    used to force the opponent to defend their stones, or to create a ko situation in which the
+    opponent is not allowed to recapture the group immediately.
+    """
+    pass
+
+def concept_liberty():
+    """
+    In the game of Go, a liberty is an empty point adjacent to a group of stones. A group of stones
+    has as many liberties as there are empty points adjacent to the group. Liberties are important
+    because they allow a group of stones to remain on the board.
+    """
+    pass
+
+def concept_territory():
+    """
+    In the game of Go, territory is an area of the board that is surrounded by stones of a single
+    color. Territory is important because it is used to determine the winner of the game.
+    """
+    pass
+
+def concept_ladder():
+    """
+    In the game of Go, a ladder is a sequence of moves in which a group of stones is chased across
+    the board. Ladders are important because they can be used to capture stones, or to defend stones
+    from capture.
+    """
+    pass
+
+def concept_seki():
+    """
+    In the game of Go, seki is a situation in which two groups of stones are adjacent to each other,
+    and neither group can capture the other. Seki is important because it can be used to prevent the
+    opponent from capturing a group of stones.
+    """
+    pass
+
+def concept_sente():
+    """
+    In the game of Go, sente is a situation in which a player is forced to respond to their opponent's
+    move. Sente is important because it can be used to force the opponent to defend their stones, or
+    to create a ko situation in which the opponent is not allowed to recapture the group immediately.
+    """
+    pass
+
+def concept_gote():
+    """
+    In the game of Go, gote is a situation in which a player is not forced to respond to their opponent's
+    move. Gote is important because it can be used to force the opponent to defend their stones, or to
+    create a ko situation in which the opponent is not allowed to recapture the group immediately.
+    """
+    pass
+
+def concept_sabaki():
+    """
+    In the game of Go, sabaki is a technique in which a player sacrifices stones in order to gain
+    a better position elsewhere on the board. Sabaki is important because it can be used to gain
+    an advantage over the opponent.
+    """
+    pass
+
+def concept_life_and_death():
+    """
+    A group of stones is said to be "alive" if it has at least two eyes, or if it cannot be captured by the opponent. 
+    A group of stones is said to be "dead" if it can be captured by the opponent.
+    """
+    pass
+
