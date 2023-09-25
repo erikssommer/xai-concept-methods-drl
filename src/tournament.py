e0c1a507ec3a67a6c14b32e934e33a15d38fe3a5
Erik Storås Sommer <erikssommer@gmail.com>
2023-09-25 21:23:08 +0200

    Running tournament

---
+++ src/tournament.py
@@ -0,0 +1,15 @@
+from utils import config
+from play import Tournament
+
+if __name__ == '__main__':
+    # Initialize the Tournament of Progressive Policies (TOPP)
+    topp = Tournament(config.nr_of_anets, config.nr_of_topp_games, config.render)
+
+    # Add the agents to the tournament
+    topp.add_agents()
+
+    # Run the tournament
+    topp.run_tournament()
+
+    # Get the results
+    topp.get_results()
