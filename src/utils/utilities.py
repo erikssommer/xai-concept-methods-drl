e9fa79971134cb7bddd24d1be028fcc936901928
Erik Storås Sommer <erikssommer@gmail.com>
2023-09-26 11:25:17 +0200

    Utility functions for distribution

--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -5,3 +5,17 @@ def normalize(x: np.array):
     Normalize the distribution
     """
     return x / np.sum(x)
+
+def softmax(x: np.array):
+    print(x)
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+def plot_distribution(distribution: np.array):
+    """
+    Plot the distribution
+    """
+    import matplotlib.pyplot as plt
+    plt.bar(np.arange(len(distribution)), distribution)
+    plt.show()
