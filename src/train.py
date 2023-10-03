cd9ca57115c5de3fb913dbed5a82942690ebfe25
Erik Storås Sommer <erikssommer@gmail.com>
2023-10-03 15:10:48 +0200

    Testing if network winns against random

--- src/train.py
+++ src/train.py
@@ -1,5 +1,5 @@
 import rl
-from utils import Timer
+from utils import Timer, setup
 import os
 import logging
 from utils import config
@@ -16,29 +16,7 @@ def train_models():
 
     # End the timer
     timer.end_timer()
-    
-def setup():
-    board_size = config.board_size
-    # Create the folder containing the models if it doesn't exist
-    if not os.path.exists('../models'):
-        os.makedirs(f'../models/')
-    if not os.path.exists(f'../models/board_size_{board_size}'):
-        os.makedirs(f'../models/board_size_{board_size}/')
-    else:
-        # Delete the model folders
-        folders = os.listdir(f'../models/board_size_{board_size}')
-        for folder in folders:
-            # Test if ends with .keras
-            if not folder.endswith('.keras'):
-                # Delete the folder even if it's not empty
-                os.system(f'rm -rf ../models/board_size_{board_size}/{folder}')
-            else:
-                # Delete the file
-                os.remove(f'../models/board_size_{board_size}/{folder}')
-    
-    # Create the folder containing the visualizations if it doesn't exist
-    if not os.path.exists('../log/visualization'):
-        os.makedirs('../log/visualization')
+
 
 def set_logging_level():
     # Create the folder containing the models if it doesn't exist
