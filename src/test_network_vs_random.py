56ec68937dd9aa7f77a4c1443925df7a98f292c7
Erik Storås Sommer <erikssommer@gmail.com>
2023-10-04 15:41:41 +0200

    Loading the model trained the longest

--- src/test_network_vs_random.py
+++ src/test_network_vs_random.py
@@ -2,6 +2,8 @@ import unittest
 import numpy as np
 import env
 from policy import ActorCriticNet
+import os
+from utils import config
 
 class TestMCTSvsRandom(unittest.TestCase):
     def test_network_as_black_vs_random(self):
@@ -9,10 +11,22 @@ class TestMCTSvsRandom(unittest.TestCase):
 
         go_env.reset()
 
-        path = '../models/board_size_5/net_10.keras'
+        # Find the model with the highest number in the name from the models/board_size_5 folder
+        path = f'../models/board_size_{config.board_size}/'
+
+        folders = os.listdir(path)
+
+        # Sort the folders by the number in the name
+        sorted_folders = sorted(folders, key=lambda x: int(x.split('_')[-1].strip('.keras')))
+
+        # Get the last folder
+        path = path + sorted_folders[-1]
+
+        print("Loading model from: {}".format(path))
+
         actor_net = ActorCriticNet(5, path)
 
-        games = 10
+        games = 100
         winns = 0
 
         for _ in range(games):
