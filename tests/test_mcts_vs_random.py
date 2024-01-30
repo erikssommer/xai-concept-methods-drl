2a2322ef866305f5011e1a50c33a9a42d6e24f48
Erik Storås Sommer <erikssommer@gmail.com>
2024-01-30 12:13:54 +0100

    Tesing model performance against random with lookahead

--- tests/test_mcts_vs_random.py
+++ tests/test_mcts_vs_random.py
@@ -7,129 +7,40 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.
 
 from env import GoEnv
 from env import govars
-from mcts import MCTS
-from policy import ActorCriticNet
+from mcts import MCTSzero as MCTS
+from policy import ConvNet, ResNet, ActorCriticNet, FastPredictor, LiteModel
 
 
 class TestMCTSvsRandom(unittest.TestCase):
+
     def test_mcts_as_black_vs_random(self):
         victories = 0
-        games = 5
-        rollouts = 50
-        board_size = 4
-
-        for _ in range(games):
-            go_env = GoEnv(size=board_size)
-            go_env.reset()
-            game_state = go_env.state()
-            curr_turn = go_env.turn()
-            tree = MCTS(game_state, 1, 1, rollouts, board_size, board_size**2*5)
-
-            terminated = False
-
-            while not terminated:
-                if curr_turn == govars.BLACK:
-                    best_action_node, _ = tree.search()
-                    _, _, terminated, _ = go_env.step(best_action_node.action)
-                    tree.set_root_node(best_action_node)
-                else:
-                    action = go_env.uniform_random_action()
-                    _, _, terminated, _ = go_env.step(action)
-                    
-                    # Update the tree, may be done better
-                    tree.set_root_node_with_action(action)
-                
-                curr_turn = go_env.turn()
-                #go_env.render()
-
-            black_won = go_env.winning()
-
-            if black_won == 1:
-                victories += 1
-            
-            #go_env.render()
-
-        # Calculate the win probability
-        win_probability = victories / games
-
-        # Print the results
-        print("Win probability as black: {}".format(win_probability))
-
-        # Assert the results
-        self.assertTrue(win_probability >= 0.9)
-    
-    def test_mcts_as_white_vs_random(self):
-        victories = 0
-        games = 5
-        rollouts = 50
-        board_size = 4
-
-        for _ in range(games):
-            go_env = GoEnv(size=board_size)
-            go_env.reset()
-            game_state = go_env.state()
-            curr_turn = go_env.turn()
-            tree = MCTS(game_state, 1, 1, rollouts, board_size, board_size**2*5)
-
-            first_action = True
-            terminated = False
-
-            while not terminated:
-                if curr_turn == govars.WHITE:
-                    best_action_node, _ = tree.search()
-                    _, _, terminated, _ = go_env.step(best_action_node.action)
-
-                    tree.set_root_node(best_action_node)
-                else:
-                    action = go_env.uniform_random_action()
-                    state, _, terminated, _ = go_env.step(action)
-
-                    # Need to set the root node to the state if it is the first move of the game
-                    if first_action:
-                        tree.set_root(state)
-                        first_action = False
-                    else:
-                        tree.set_root_node_with_action(action)
-                
-                curr_turn = go_env.turn()
-                #go_env.render()
-
-            black_won = go_env.winning()
-
-            if black_won != 1:
-                victories += 1
-            
-            #go_env.render()
-
-        # Calculate the win probability
-        win_probability = victories / games
-
-        # Print the results
-        print("Win probability as white: {}".format(win_probability))
-
-        # Assert the results
-        self.assertTrue(win_probability >= 0.9)
-
-    def test_mcts_as_black_with_only_critic_vs_random(self):
-        victories = 0
-        games = 5
-        rollouts = 50
+        games = 10
+        simulations = 100
         board_size = 5
-        path = '../models/saved_sessions/board_size_5/starship/net_1050.keras'
-        policy = ActorCriticNet(board_size, path)
+        move_cap = board_size**2 * 4
+        komi = 0.5
+        c = 1.3
+        det_moves = 0
+        path = '../models/saved_sessions/board_size_5/blueorigin/net_10000.keras'
+        policy = ConvNet(board_size, path)
+
+        model = FastPredictor(LiteModel.from_keras_model(policy.model))
 
         for _ in range(games):
-            go_env = GoEnv(size=board_size)
+            go_env = GoEnv(size=board_size, komi=komi)
             go_env.reset()
-            game_state = go_env.state()
-            curr_turn = go_env.turn()
-            tree = MCTS(game_state, 1, 0, rollouts, board_size, board_size**2*5, policy_nn=policy)
+            game_state = go_env.canonical_state()
+            tree = MCTS(game_state, simulations, board_size, move_cap, model, c, komi, det_moves)
+
+            curr_turn = 0
+            move_nr = 0
 
             terminated = False
 
             while not terminated:
                 if curr_turn == govars.BLACK:
-                    best_action_node, _ = tree.search()
+                    best_action_node, _ = tree.search(move_nr)
                     _, _, terminated, _ = go_env.step(best_action_node.action)
                     tree.set_root_node(best_action_node)
                 else:
@@ -139,15 +50,14 @@ class TestMCTSvsRandom(unittest.TestCase):
                     # Update the tree, may be done better
                     tree.set_root_node_with_action(action)
                 
-                curr_turn = go_env.turn()
-                #go_env.render()
+                curr_turn = 1 - curr_turn
+                move_nr += 1
+                go_env.render()
 
             black_won = go_env.winning()
 
             if black_won == 1:
                 victories += 1
-            
-            #go_env.render()
 
         # Calculate the win probability
         win_probability = victories / games
@@ -162,24 +72,32 @@ class TestMCTSvsRandom(unittest.TestCase):
     def test_mcts_as_white_with_only_critic_vs_random(self):
         victories = 0
         games = 10
-        rollouts = 50
+        simulations = 100
         board_size = 5
-        path = '../models/saved_sessions/board_size_5/starship/net_1050.keras'
-        policy = ActorCriticNet(board_size, path)
+        move_cap = board_size**2 * 4
+        komi = 0.5
+        c = 1.3
+        det_moves = 0
+        path = '../models/saved_sessions/board_size_5/blueorigin/net_10000.keras'
+        policy = ConvNet(board_size, path)
+
+        model = FastPredictor(LiteModel.from_keras_model(policy.model))
 
         for _ in range(games):
             go_env = GoEnv(size=board_size)
             go_env.reset()
-            game_state = go_env.state()
-            curr_turn = go_env.turn()
-            tree = MCTS(game_state, 1, 0, rollouts, board_size, board_size**2*5, policy_nn=policy)
+            game_state = go_env.canonical_state()
+            tree = MCTS(game_state, simulations, board_size, move_cap, model, c, komi, det_moves)
+
+            curr_turn = 0
+            move_nr = 0
             
             first_action = True
             terminated = False
 
             while not terminated:
                 if curr_turn == govars.WHITE:
-                    best_action_node, _ = tree.search()
+                    best_action_node, _ = tree.search(move_nr)
                     _, _, terminated, _ = go_env.step(best_action_node.action)
                     tree.set_root_node(best_action_node)
                 else:
@@ -188,20 +106,19 @@ class TestMCTSvsRandom(unittest.TestCase):
                     
                     # Need to set the root node to the state if it is the first move of the game
                     if first_action:
-                        tree.set_root(state)
+                        tree.set_root(state, 1)
                         first_action = False
                     else:
                         tree.set_root_node_with_action(action)
                 
-                curr_turn = go_env.turn()
-                #go_env.render()
+                curr_turn = 1 - curr_turn
+                move_nr += 1
+                go_env.render()
 
             black_won = go_env.winning()
 
-            if black_won != 1:
+            if black_won == -1:
                 victories += 1
-            
-            #go_env.render()
 
         # Calculate the win probability
         win_probability = victories / games
