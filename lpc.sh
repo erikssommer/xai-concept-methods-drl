4526bd3c5c53e24288f657d6aa8a9bb9d5248f78
Erik Storås Sommer <erikssommer@gmail.com>
2024-02-01 13:41:58 +0100

    Running as single thread

--- lpc.sh
+++ lpc.sh
@@ -13,8 +13,8 @@
 
 WORKDIR=${SLURM_SUBMIT_DIR}
 cd ${WORKDIR}
-echo "we are running from this directory: $SLURM_SUBMIT_DIR"
-echo " the name of the job is: $SLURM_JOB_NAME"
+echo "Running from this directory: $SLURM_SUBMIT_DIR"
+echo "The name of the job is: $SLURM_JOB_NAME"
 echo "Th job ID is $SLURM_JOB_ID"
 echo "The job was run on these nodes: $SLURM_JOB_NODELIST"
 echo "Number of nodes: $SLURM_JOB_NUM_NODES"
@@ -25,4 +25,4 @@ echo "Total of $SLURM_NTASKS cores"
 module purge
 module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 cd src/
-mpirun python train_single_thread.py
+python train_single_thread.py
