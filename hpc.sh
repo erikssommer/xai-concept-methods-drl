f90131d6e64bdedbac5a0d3713bc3856c83b1126
erikssommer <erikssommer@gmail.com>
2024-02-01 14:02:56 +0100

    Printing info about run

--- hpc.sh
+++ hpc.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-#SBATCH --partition=CPUQ
+#SBATCH --partition=GPUQ
 #SBATCH --account=share-ie-idi
 #SBATCH --time=2-00:00:00
-#SBATCH --nodes=1             # 1 compute nodes
-#SBATCH --ntasks-per-node=10    # 10 mpi process each node
+#SBATCH --nodes=1              # 1 compute nodes
+#SBATCH --ntasks-per-node=10   # 10 mpi process each node
 #SBATCH --mem=12000            # 12GB - in megabytes
-#SBATCH --job-name="hpc job"
+#SBATCH --gres=gpu:p100:1      # 1 tesla gpu
+#SBATCH --job-name="hpc-job"
 #SBATCH --output=log.txt
 #SBATCH --mail-user=erik.s.sommer@ntnu.no
 #SBATCH --mail-type=ALL
 
 WORKDIR=${SLURM_SUBMIT_DIR}
 cd ${WORKDIR}
-echo "we are running from this directory: $SLURM_SUBMIT_DIR"
-echo " the name of the job is: $SLURM_JOB_NAME"
+echo "Running from this directory: $SLURM_SUBMIT_DIR"
+echo "The name of the job is: $SLURM_JOB_NAME"
 echo "Th job ID is $SLURM_JOB_ID"
 echo "The job was run on these nodes: $SLURM_JOB_NODELIST"
 echo "Number of nodes: $SLURM_JOB_NUM_NODES"
@@ -22,7 +23,6 @@ echo "We are using $SLURM_CPUS_ON_NODE cores per node"
 echo "Total of $SLURM_NTASKS cores"
 
 module purge
-module load fosscuda/2019a
-module load TensorFlow/1.13.1-Python-3.7.2
+module load TensorFlow/2.11.0-foss-2022a-CUDA-11.7.0
 cd src/
-mpirun python3 train_hpc.py
+mpirun python train_hpc.py
