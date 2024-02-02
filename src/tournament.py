0a9605f70c46bfc44d673e8cc3ef23cbaac0633c
Erik Storås Sommer <erikssommer@gmail.com>
2024-02-02 10:06:32 +0100

    Test models on idun

--- src/tournament.py
+++ src/tournament.py
@@ -2,11 +2,20 @@ from utils import config
 from topp import Topp
 
 if __name__ == '__main__':
+    number_of_games = 2
+    dirs = ['saved_sessions', 'model_performance', 'training']
+    version = ['endeavor', 'falcon', 'starship']
+    greedy_move = True
+    board_size = 5
+    render = True
+    canonical = True
+    convnet = True
+
     # Initialize the Tournament of Progressive Policies (TOPP)
-    topp = Topp(config.board_size, config.nr_of_topp_games, config.render)
+    topp = Topp(board_size, number_of_games, render=render, komi=0.5, dir=dirs[2], version=version[2], canonical=canonical)
 
     # Add the agents to the tournament
-    topp.add_agents()
+    topp.add_agents(greedy_move, convnet)
 
     # Run the tournament
     topp.run_tournament()
@@ -15,5 +24,4 @@ if __name__ == '__main__':
     topp.get_results()
 
     # Plot the results
-    if config.plot:
-        topp.plot_results(True)
+    #topp.plot_results()
