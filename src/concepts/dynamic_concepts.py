6676c2f7601645d3c9d564543d982a35199e536c
Erik Storås Sommer <erikssommer@gmail.com>
2024-02-09 16:13:32 +0100

    Dynamic concept for end_game and life_and_death

--- src/concepts/dynamic_concepts.py
+++ src/concepts/dynamic_concepts.py
@@ -11,8 +11,17 @@ Dynamic concepts are concepts that are not fixed, but rather change over time in
 
 
 class DynamicConcepts:
-    def __init__(self, init_state, simulations, board_size, concept_type_single, path, move_cap=100, random_subpar=True):
-        # 'both' or 'single'. Single means concepts where only one player is considered, both means concepts where both players are considered.
+    def __init__(self,
+                 init_state,
+                 simulations,
+                 board_size,
+                 concept_type_single,
+                 path,
+                 move_cap=100,
+                 random_subpar=True):
+        # 'both' or 'single'.
+        # Single means concepts where only one player is considered
+        # Both means concepts where both players are considered.
         self.concept_type_single = concept_type_single
         self.board_size = board_size
         self.random_subpar = random_subpar
@@ -35,7 +44,7 @@ class DynamicConcepts:
         min_value_diff = 0.2
 
         if self.concept_type_single:
-            # This is the double of 'both' concept type due to the fact that the opponent's turn is skipped
+            # This is the double of 'both' concept type due to the fact that the opponent's turn is skipped.
             t_maximum_rollout_depth = 20
             maximum_depth_find_sub_rollout = t_maximum_rollout_depth - 10
         else:
@@ -70,10 +79,12 @@ class DynamicConcepts:
 
             # From the subpar node, perform a optimal rollout to the maximum depth
             if node.time_step < maximum_depth_find_sub_rollout:
-                # Find the subpar next state with a minimum value difference of 0.20 and/or a visit count difference of 10% of the highest visit count
+                # Find the subpar next state with a minimum value difference of 0.20 and/or
+                # a visit count difference of 10% of the highest visit count
                 sub_par_children: list[Node] = []
                 for child in node.children:
-                    if child.n_visit_count < highest_visit_count * (1-min_visit_count_diff) or child.q_value() < next_optimal_node.q_value() - min_value_diff:
+                    if child.n_visit_count < highest_visit_count * (1 - min_visit_count_diff) \
+                            or child.q_value() < next_optimal_node.q_value() - min_value_diff:
                         sub_par_children.append(child)
 
                 # If the subpar children list is empty, break the loop
@@ -84,7 +95,8 @@ class DynamicConcepts:
                     sub_par_children)
 
                 # Assert that the best subpar state is has a lower visit count than the optimal state
-                assert best_subpar_node.n_visit_count < next_optimal_node.n_visit_count or best_subpar_node.q_value() < next_optimal_node.q_value()
+                assert best_subpar_node.n_visit_count < next_optimal_node.n_visit_count \
+                    or best_subpar_node.q_value() < next_optimal_node.q_value()
 
                 if best_subpar_node.predict_state_rep is not None:
                     subpar_rollout_states.append(
@@ -97,8 +109,8 @@ class DynamicConcepts:
                     if self.random_subpar:
                         optimal_child = np.random.choice(curr_node.children)
                     else:
-                        optimal_child, _ = self.find_best_next_node(curr_node.children)
-
+                        optimal_child, _ = self.find_best_next_node(
+                            curr_node.children)
 
                     if optimal_child:
                         if optimal_child.predict_state_rep is not None:
@@ -147,11 +159,136 @@ class DynamicConcepts:
     """
 
     @staticmethod
-    def opening_play(board_size, komi=1.5):
+    def opening_play(board_size):
         """
-        Concept of starting from an empty board
+        This is the initial phase of the game where players try to establish their initial structures and influence on the board. 
+        The goal is to control as much territory as possible and set up for the middle game.
+        """
+        concept_type_single = False
+        go_env = env.GoEnv(board_size)
+        go_env.reset()
+        return go_env.canonical_state(), concept_type_single
+
+    @staticmethod
+    def end_game(board_size):
+        """
+        The end game is the final phase of the game where players try to secure their territories and capture their opponent's stones.
+        """
+        concept_type_single = False
+        # Initialize all planes to zeros
+        game_state = np.zeros((6, board_size, board_size))
+
+        # Set up an endgame scenario
+        for i in range(board_size):
+            for j in range(board_size):
+                if i < board_size / 2 and j < board_size / 2:
+                    game_state[0, i, j] = 1  # Current player's territory
+                elif i >= board_size / 2 and j >= board_size / 2:
+                    game_state[1, i, j] = 1  # Opponent player's territory
+
+        # Set the third plane to represent the current player's turn
+        game_state[2, :, :] = 0
+
+        # Set the fourth plane to represent invalid moves (places where there are stones)
+        # Loop through the board and set the fourth plane to 1 where there are stones
+        for i in range(board_size):
+            for j in range(board_size):
+                if game_state[0, i, j] == 1 or game_state[1, i, j] == 1:
+                    game_state[3, i, j] = 1
+        
+        # Set the fifth plane to represent that the previous move was not a pass
+        game_state[4, :, :] = 0
+
+        # Set the last plane to represent that the game is not over
+        game_state[5, :, :] = 0
+
+        return game_state, concept_type_single
+
+    @staticmethod
+    def life_and_death(board_size, komi=1.5):
+        """
+        Concept of starting from a board with a few stones
+        """
+        concept_type_single = False
+        # Initialize all planes to zeros
+        game_state = np.zeros((6, board_size, board_size))
+
+        # Set up a scenario where Player 1 has a group of stones under threat
+        game_state[0, board_size // 2 +1, board_size // 2 +1] = 1  # Current player's stone
+        game_state[1, board_size // 2, board_size // 2 + 1] = 1  # Opponent player's stone
+        game_state[1, board_size // 2 + 1, board_size // 2] = 1  # Opponent player's stone
+        game_state[1, board_size // 2 - 1, board_size // 2] = 1  # Opponent player's stone
+        game_state[1, board_size // 2, board_size // 2 - 1] = 1  # Opponent player's stone
+
+        # Set the third plane to represent the current player's turn
+        game_state[2, :, :] = 1
+
+        # Set the fourth plane to represent invalid moves (all territories are claimed)
+        for i in range(board_size):
+            for j in range(board_size):
+                if game_state[0, i, j] == 1 or game_state[1, i, j] == 1:
+                    game_state[3, i, j] = 1
+        
+        # If ko is present, set a ko stone
+        game_state[3, board_size // 2, board_size // 2] = 1  # Current player's stone
+
+        # Set the fifth plane to represent that the previous move was not a pass
+        game_state[4, :, :] = 0
+
+        # Set the last plane to represent that the game is not over
+        game_state[5, :, :] = 0
+
+        print(game_state)
+
+        return game_state, concept_type_single
+
+
+    @staticmethod
+    def keep_initiative(board_size, komi=1.5):
+        """
+        Concept of starting from a board with a few stones
         """
         concept_type_single = False
         go_env = env.GoEnv(board_size, komi)
         go_env.reset()
+
+        # Set up a scenario where Player 1 has the initiative
+        go_env.state[board_size // 2, board_size // 2] = 1  # Player 1's stone
+        go_env.state[board_size // 2, board_size // 2 + 1] = -1  # Player 2's stone
+
+        return go_env.canonical_state(), concept_type_single
+
+    @staticmethod
+    def ko_fight(board_size, komi=1.5):
+        """
+        A Ko fight involves a sequence of moves elsewhere on the board (Ko threats) that aim to make the opponent respond so that the player can retake the Ko.
+        """
+        concept_type_single = False
+        go_env = env.GoEnv(board_size, komi)
+        go_env.reset()
+
+        # Set up a Ko fight scenario
+        go_env.state[board_size // 2, board_size // 2] = 1  # Player 1's stone
+        go_env.state[board_size // 2, board_size // 2 + 1] = -1  # Player 2's stone
+        go_env.state[board_size // 2 + 1, board_size // 2] = -1  # Player 2's stone
+        go_env.state[board_size // 2 + 1, board_size // 2 + 1] = 1  # Player 1's stone
+
+        return go_env.canonical_state(), concept_type_single
+
+    @staticmethod
+    def invasion_and_reduction(board_size, komi=1.5):
+        """
+        These are strategies used to disrupt your opponent's territories. 
+        An invasion is a sequence of moves that attempts to establish a live group inside an opponent's territory, 
+        while a reduction is a move or sequence of moves that attempts to reduce the potential size
+        of an opponent's territory without necessarily trying to live inside.
+        """
+        concept_type_single = False
+        go_env = env.GoEnv(board_size, komi)
+        go_env.reset()
+        # Set up a scenario where Player 1 has a large territory
+        for i in range(board_size // 2):
+            for j in range(board_size):
+                go_env.state[i, j] = 1  # Player 1's stones
+
         return go_env.canonical_state(), concept_type_single
