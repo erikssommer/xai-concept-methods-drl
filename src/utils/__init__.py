f29c934e473a7ab3816465051bcea0c1994b3357
Erik Storås Sommer <erikssommer@gmail.com>
2024-02-19 13:29:18 +0100

    Util function for concept folder setup

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,5 +1,5 @@
 from .read_config import config
 from .timer import Timer
 from .utilities import *
-from .setup import folder_setup
+from .setup import folder_setup, concept_folder_setup_and_score
 from .tensorboard import tensorboard_setup, write_to_tensorboard
--- src/utils/setup.py
+++ src/utils/setup.py
@@ -1,4 +1,8 @@
 import os
+import random
+import string
+import json
+
 from utils import config
 
 def folder_setup(model_type):
@@ -23,3 +27,27 @@ def folder_setup(model_type):
             else:
                 # Delete the file
                 os.remove(f'../models/training/{model_type}/board_size_{board_size}/{folder}')
+
+def concept_folder_setup_and_score(concept_type, model_type, board_name, session_name, concept_name, name, score):
+    # Remove the files if they exist
+    if os.path.exists(f"../concept_presences/{concept_type}/{model_type}/{board_name}/{session_name}/{concept_name}/"):
+        # Test if epoch folder exists
+        if os.path.exists(f"../concept_presences/{concept_type}/{model_type}/{board_name}/{session_name}/{concept_name}/{name}"):
+            # Remove all files in the epoch folder
+            # Fist save the 
+            for file in os.listdir(f"../concept_presences/{concept_type}/{model_type}/{board_name}/{session_name}/{concept_name}/{name}"):
+                os.remove(f"../concept_presences/{concept_type}/{model_type}/{board_name}/{session_name}/{concept_name}/{name}/{file}")
+
+    os.makedirs("../concept_presences", exist_ok=True)
+    os.makedirs("../concept_presences/{concept_type}", exist_ok=True)
+    os.makedirs(f"../concept_presences/{concept_type}/{model_type}", exist_ok=True)
+    os.makedirs(f"../concept_presences/{concept_type}/{model_type}/{board_name}", exist_ok=True)
+    os.makedirs(f"../concept_presences/{concept_type}/{model_type}/{board_name}/{session_name}", exist_ok=True)
+    os.makedirs(f"../concept_presences/{concept_type}/{model_type}/{board_name}/{session_name}/{concept_name}", exist_ok=True)
+    os.makedirs(f"../concept_presences/{concept_type}/{model_type}/{board_name}/{session_name}/{concept_name}/{name}", exist_ok=True)
+
+    random_suffix = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+
+    # Save concept presences in json file
+    with open(f"../concept_presences/{concept_type}/{model_type}/{board_name}/{session_name}/{concept_name}/{name}/{random_suffix}.json", "w") as f:
+        json.dump(score, f)
