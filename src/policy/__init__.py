290e38209ae9c97e01184637e8bf01e826b9363b
Erik Storås Sommer <erikssommer@gmail.com>
2024-03-11 10:28:37 +0100

    Added some typing and docstr

--- src/policy/__init__.py
+++ src/policy/__init__.py
@@ -5,13 +5,14 @@ from .fast_predictor import FastPredictor
 from .lite_model import LiteModel
 from .conceptnet import ConceptNet
 
-def get_policy(model_type, board_size, nr_of_concepts=0, path=None) -> BaseNet:
+def get_policy(model_type: int, board_size: int, nr_of_concepts: int = 0, path: str = None) -> BaseNet:
     """
     Get the policy model
     
     Args:
         model_type (str): The type of model to use
         board_size (int): The size of the board
+        nr_of_concepts (int): The number of concepts to use (only for conceptnet)
         path (str): The path to the model
         
     Returns:
