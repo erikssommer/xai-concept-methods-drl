8bb6037be354b8d5e156c9504eec82c63ee912cb
Erik Storås Sommer <erikssommer@gmail.com>
2024-03-18 11:14:50 +0100

    Update reward value for ONE_EYE explanation

--- src/jem/explanations.py
+++ src/jem/explanations.py
@@ -7,7 +7,7 @@ class Explanations(Enum):
     Enum class for explanations and their corresponding reward values
     """
     NULL = "a generic move not tied to a strategy", 0
-    ONE_EYE = "creates one eye where the opponent cannot place a stone", 0.1
+    ONE_EYE = "creates one eye where the opponent cannot place a stone", 0.3
     TWO_EYES = "creates two eyes resulting in forming a living group", 0.5
     CAPTURE_A_STONE = "captures one of the opponent's stones", 0.2
     CAPTURE_GROUP_OF_STONES = "captures a group of the opponent's stones", 0.5
